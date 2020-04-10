ab5ad7e8b5035291c2b1f827386621f5beab4c04
D4RKSH4D0WS <49472584+D4RKSH4D0WS@users.noreply.github.com>
2020-04-10 09:53:58 +0700

    Update wttr.py

--- wttr.py
+++ wttr.py
@@ -1,12 +1,5 @@
-#Hai bangsat
 #Mau rikod ? Boleh asal jgn hapus author ya asu !
 #Author: D4RKSH4D0WS
-import requests,re
-se=raw_input('Input: ')
-a=requests.get('http://wttr.in/'+se+'?format="%l:+%t+%C+%h+%w"').text
-b=a.replace('"','')
-print
-print 'Keterangan:'
-print '[lokasi]: [suhu] [infocuaca] [kelembapan] [arahangin]'
-print
-print b
+import os
+kota=raw_input('Masukkan lokasi: ')
+os.system('curl -s http://wttr.in/'+kota+' | sed -n \"1,7p\"')
